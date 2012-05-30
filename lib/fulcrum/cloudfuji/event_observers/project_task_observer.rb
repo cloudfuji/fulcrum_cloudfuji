ce424974a84402881c404e0afbac4c3ff41d4988
Nathan Broadbent <nathan.f77@gmail.com>
2012-05-31 10:41:17 +1200

    Fixed requested_by and owned_by assignment in ProjectTaskObserver

--- lib/fulcrum/cloudfuji/event_observers/project_task_observer.rb
+++ lib/fulcrum/cloudfuji/event_observers/project_task_observer.rb
@@ -17,8 +17,8 @@ module Fulcrum
           story.story_type      = data['task_type']
           story.state           = data['state']
           story.accepted_at     = data['accepted_at']
-          story.requested_by_id = User.find_by_ido_id(data['requested_by_id'])
-          story.owned_by_id     = User.find_by_ido_id(data['owned_by_id'])
+          story.requested_by    = User.find_by_ido_id(data['requested_by_id'])
+          story.owned_by        = User.find_by_ido_id(data['owned_by_id'])
           story.project         = Project.find_by_ido_id(data['project_id'])
           story.labels          = data['labels']
 
